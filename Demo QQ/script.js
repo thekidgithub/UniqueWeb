6b2ec088ee753e03986ec732c60de3cf6b87b0b4
thekidgithub <64016518+thekidgithub@users.noreply.github.com>
2023-07-13 12:01:41 +0800

    Add files via upload

---
+++ Demo QQ/script.js
@@ -0,0 +1,121 @@
+const input = document.querySelector('.input');
+const send = document.querySelector('.send');
+const groupText = document.querySelector('.chat-text');
+const image = document.querySelector('.image');
+const groupApps = document.querySelector('.group-apps');
+const groupAppsLis = document.querySelectorAll('.group-apps li');
+const modelBlock = document.querySelector('.model-block');
+const models = document.querySelectorAll('.model');
+const buttonsOfModelBlock = document.querySelectorAll('.modelbutton');
+const container = document.querySelector('.container');
+const more = document.querySelector('.more');
+const moreSettings = document.querySelector('.more-settings');
+updButton();
+
+input.addEventListener('keydown', (e) => {//输入文本时，对不同的按键决定换行/发送
+    if (e.shiftKey && e.key == 'Enter' || e.altKey && e.key == 'Enter') {
+        e.preventDefault();
+    }
+    else if (e.ctrlKey && e.key == 'Enter') {
+        input.value += '\n';
+    }
+    else if (e.key == 'Enter') {
+        e.preventDefault();
+        if (input.value.trim() != '') sendText();
+    }
+    updButton();
+})
+
+send.addEventListener('click', () => {//发送按钮
+    if (input.value.trim() != '') sendText();
+})
+
+image.addEventListener('click', sendImage);//发送图片
+
+function updButton() {//更新按钮的状态（输入框内是否有文本）
+    if (input.value.trim() != '') send.classList.add('hastext');
+    else send.classList.remove('hastext');
+}
+
+function sendText() {//发送一个新的文本块（清空输入框，在消息框渲染一个新的块）
+    let text = input.value;
+    input.value = '';
+    updButton();
+    const newText = document.createElement('div');
+    const profile = document.createElement('div');
+    const content = document.createElement('div');
+    const id = document.createElement('div');
+    const textContent = document.createElement('div');
+    newText.classList.add('newtext');
+    profile.classList.add('newprofile');
+    content.classList.add('content');
+    id.classList.add('id');
+    textContent.classList.add('textcontent');
+    id.innerHTML = 'thekid';
+    textContent.innerHTML = text;
+    content.appendChild(id);
+    content.appendChild(textContent);
+    newText.appendChild(content);
+    newText.appendChild(profile);
+    groupText.appendChild(newText);
+}
+
+function sendImage() {//发送图片
+    const newText = document.createElement('div');
+    const profile = document.createElement('div');
+    const content = document.createElement('div');
+    const id = document.createElement('div');
+    const fileContent = document.createElement('input');
+    const imgContent = document.createElement('img');
+    //这段我之前没有学过，通过查阅资料写的
+    fileContent.type = 'file';
+    fileContent.click();
+    fileContent.addEventListener('change', function (e) {
+        let file = e.target.files[0];
+        if (file) {
+            let reader = new FileReader();
+            reader.onload = function (e) {
+                imgContent.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+        groupText.appendChild(newText);
+    });
+    newText.classList.add('newtext');
+    profile.classList.add('newprofile');
+    content.classList.add('content');
+    id.classList.add('id');
+    imgContent.classList.add('imgcontent');
+    id.innerHTML = 'thekid';
+    content.appendChild(id);
+    content.appendChild(imgContent);
+    newText.appendChild(content);
+    newText.appendChild(profile);
+
+}
+
+groupApps.addEventListener('click', () => {//群应用下拉框
+    groupAppsLis.forEach(e => {
+        e.classList.toggle('show');
+    })
+})
+//打开/关闭模态框
+models.forEach(e => {
+    e.addEventListener('click', () => {
+        modelBlock.classList.add('show');
+        modelBlock.classList.remove('hidden');
+        container.classList.add('show');
+    })
+})
+
+buttonsOfModelBlock.forEach(e => {
+    e.addEventListener('click', () => {
+        modelBlock.classList.remove('show');
+        modelBlock.classList.add('hidden');
+        container.classList.remove('show');
+    })
+})
+//拉起“更多”框
+more.addEventListener('click', () => {
+    moreSettings.classList.toggle('show');
+})
